21c53a536b54d99c5d6ce765068b6b89ef63fe68
Michael Müller <michael.mueller@mychie2.net>
2014-09-05 23:01:17 +0200

    structered cmd line options and introduced new ones

--- main.py
+++ main.py
@@ -69,10 +69,28 @@ cache_file = join(app_path, "cache.ics")
 filter_file = join(app_path, "filters")
 
 parser = argparse.ArgumentParser(description='A remote ical aggregator.')
-# TODO implement week, month
-parser.add_argument('-t', dest='target', choices=['day'], default='day', help='display time frame')
-parser.add_argument('-o', dest='offset', metavar='n', type=int, action='store', default=0, help='numerical offset from today, this week, or this month')
+parser.add_argument('-d', dest='day', action='store_true', default=False, help='Display single day (default).')
+parser.add_argument('-w', dest='week', action='store_true', default=False, help='Display week.')
+parser.add_argument('-m', dest='month', action='store_true', default=False, help='Display month.')
+parser.add_argument('-n', dest='nop', action='store_true', default=False, help='Don\'t show any output. Useful in combination with -r.')
+parser.add_argument('-o', dest='offset', metavar='n', type=int, action='store', default=0, help='Numerical offset from today, this week, or this month.')
+parser.add_argument('-r', dest='force_retrieve', action='store_true', default=False, help='Force retrieving remote icals.')
+parser.add_argument('--no-retrieve', dest='no_retrieve', action='store_true', default=False, help='Don\'t allow retrieving remote icals, abort if no cache file is present. This overrides -r.')
+
 args = parser.parse_args()
+
+# check for argument conflicts
+if sum([ args.day, args.week, args.month ]) > 1:
+  print "Error: conflicting flags (",
+  if args.day:
+    print "-d",
+  if args.week:
+    print "-w",
+  if args.month:
+    print "-m",
+
+  print "). Aborting."
+  exit(1)
 
 
 # load urls
@@ -83,24 +101,31 @@ with open(join(app_path, "urls")) as f:
 
 # retrieve icals if neccessary
 
-if (not os.path.exists(cache_file)
-  or time.time() - os.path.getmtime(cache_file) > ical_age_limit):
-  print "Retrieving icals...",
-  
-  f = open(cache_file, "wb")
-  cal_string = ""
+if not args.no_retrieve:
 
-  for url in urls:
-    # TODO catch errors
-    url = url.rstrip()
-    u = urllib.urlopen(url, url_file)
-    buf = u.read()
-    cal_string += buf
-    f.write(buf)
+  if (args.force_retrieve
+    or not os.path.exists(cache_file)
+    or time.time() - os.path.getmtime(cache_file) > ical_age_limit):
+    print "Retrieving icals...",
+    
+    f = open(cache_file, "wb")
+    cal_string = ""
 
-  f.close()
+    for url in urls:
+      # TODO catch errors
+      url = url.rstrip()
+      u = urllib.urlopen(url, url_file)
+      buf = u.read()
+      cal_string += buf
+      f.write(buf)
 
-  print "done."
+    f.close()
+
+    print "done."
+
+elif not os.path.exists(cache_file):
+  print "Cache file doesn't exist, aborting."
+  exit(1)
 
 else:
 
@@ -109,12 +134,19 @@ else:
   f.close()
 
 
+# nop option
+
+if args.nop:
+  exit(0)
+
+
 # load cals
 
 cals = icalendar.Calendar.from_ical(cal_string, multiple=True)
 
 
 # import filter rules
+
 with open(filter_file) as f:
   filters = f.readlines()
 
@@ -124,10 +156,22 @@ for i, elem in enumerate(filters):
 
 # display
 
-if args.target == 'day':
+if args.month:
+
+  print "Not implemented yet. Sorry."
+  exit(1336)
+
+elif args.week:
+
+  print "Not implemented yet. Sorry."
+  exit(1336)
+
+else:
+
   if args.offset == 0:
     print events_today(cals)
   else:
     target = dt.date.today() + dt.timedelta(days=args.offset)
     print events_date(cals, target)
 
+exit(0)
